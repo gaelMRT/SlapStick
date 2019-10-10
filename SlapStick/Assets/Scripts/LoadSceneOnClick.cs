e27f42f5eaf94c461faad549bec19a03defcaa00
brunazzir <34240598+brunazzir@users.noreply.github.com>
2019-10-10 16:11:53 +0200

    MainMenu Additions
    Added basic functionalities

---
+++ SlapStick/Assets/Scripts/LoadSceneOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    
+    
+    /*// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
