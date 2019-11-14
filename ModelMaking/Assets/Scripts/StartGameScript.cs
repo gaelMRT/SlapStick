2be5ea47545196cb8513a864ef9b5d5014f8c9aa
brunazzir <34240598+brunazzir@users.noreply.github.com>
2019-11-14 16:11:23 +0100

    End of day commitn
    added scene changes and components for laser pointer

---
+++ ModelMaking/Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChangeSceneOnStartButtonClick(string SceneName)
+    {
+        Application.LoadLevel(SceneName);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ModelMaking/Assets/Scripts/ViveInteractionWithMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Valve.VR.Extras;
+
+
+public class Menu_Controller_Interaction : MonoBehaviour
+{
+    void Awake()
+    {
+        laserPointer.PointerIn += PointerInside;
+        laserPointer.PointerOut += PointerOutside;
+        laserPointer.PointerClick += PointerClick;
+    }
+
+    public void PointerClick(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "AudioButton")
+        {
+            Debug.Log("Start was clicked");
+        }
+        else if (e.target.name == "PlayerOptionsButton")
+        {
+            Debug.Log("Player Options was clicked");
+        }
+    }
+
+    public void PointerInside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "AudioButton")
+        {
+            Debug.Log("Start was entered");
+        }
+        else if (e.target.name == "PlayerOptionsButton")
+        {
+            Debug.Log("Player Options was entered");
+        }
+    }
+
+    public void PointerOutside(object sender, PointerEventArgs e)
+    {
+        if (e.target.name == "AudioButton")
+        {
+            Debug.Log("Start was exited");
+        }
+        else if (e.target.name == "PlayerOptionsButton")
+        {
+            Debug.Log("Player Options was exited");
+        }
+    }
+}
